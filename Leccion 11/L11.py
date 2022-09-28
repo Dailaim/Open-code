c1e65565d0cf2e00e9ef966ce8d515eb79b77e91
Daizai <leo-im@hotmail.com>
2022-09-27 23:48:58 -0500

    11

---
+++ Leccion 11/L11.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+conecion = sqlite3.connect('./alumnos.db')
+cursor =conecion.cursor()
+
+cursor.execute("CREATE TABLE Alumnos(id INTEGER PRIMARY KEY AUTOINCREMENT,nombre TEXT NOT NULL,apellido TEXT NOT NULL);")
+
+cursor.execute("INSERT INTO Alumnos(nombre,apellido) VALUES('pabo', 'guerrero')")
+cursor.execute("INSERT INTO Alumnos(nombre,apellido) VALUES('marco', 'Rodriguez')")
+cursor.execute("INSERT INTO Alumnos(nombre,apellido) VALUES('polo', 'Martinez')")
+cursor.execute("INSERT INTO Alumnos(nombre,apellido) VALUES('R2t2', 'Perez')")
+cursor.execute("INSERT INTO Alumnos(nombre,apellido) VALUES('Catrina', 'Mongomerri')")
+cursor.execute("INSERT INTO Alumnos(nombre,apellido) VALUES('Otrama', 'Gonzales')")
+cursor.execute("INSERT INTO Alumnos(nombre,apellido) VALUES('Marzo', 'Otorres')")
+cursor.execute("INSERT INTO Alumnos(nombre,apellido) VALUES('Martacosa', 'Iglesias')")
+conecion.commit()
+
+cursor.execute("SELECT * FROM Alumnos WHERE nombre = 'Marzo'")
+
+rows = cursor.fetchall()
+
+
+print(rows)
+conecion.close()
